8f08e641c1852d3abde1b5248add43895adef050
Thiago Abreu <thiagoa7@gmail.com>
2014-01-21 17:34:29 -0300

    Menu Activity

---
+++ src/com/example/transporte/MenuActivity.java
@@ -0,0 +1,30 @@
+package com.example.transporte;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends Activity {
+
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
+		
+		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+	}
+
+	public void callCadastro(View v) {
+		Intent i = new Intent(this, Cadastro.class);
+		startActivity(i);
+	}
+
+}
